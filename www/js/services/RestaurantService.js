f2e61e58b0949fefaf0da3b38468c3fd408a78bc
Ovidiu Bularda <ovi.b.1988@gmail.com>
2015-11-02 13:44:12 +0200

    Fix mock data

--- www/js/services/RestaurantService.js
+++ www/js/services/RestaurantService.js
@@ -6,38 +6,38 @@ angular.module('ServicesModule').factory('RestaurantsService', [ function() {
     {
       name : "McDonalds",
       img: "https://lh4.googleusercontent.com/-Ykwm0lAAjBc/VgE-ZL3PeMI/AAAAAAAAAoY/-2PvWh7vKKU/s648/mcd_800x800.jpg",
-      rating: 4,
-      address:,
+      rating: 1,
+      address: '',
       comments: []
     },
     {
       name : "KFC",
       img: "https://lh4.googleusercontent.com/-Ykwm0lAAjBc/VgE-ZL3PeMI/AAAAAAAAAoY/-2PvWh7vKKU/s648/mcd_800x800.jpg",
       rating: 4,
-      address:,
+      address: '',
       comments: []
     },
     {
       name : "La mama",
       img: "https://lh4.googleusercontent.com/-Ykwm0lAAjBc/VgE-ZL3PeMI/AAAAAAAAAoY/-2PvWh7vKKU/s648/mcd_800x800.jpg",
-      rating: 4,
-      address:,
+      rating: 5,
+      address: '',
       comments: []
       
     },
     {
       name : "Subway",
       img: "https://lh4.googleusercontent.com/-Ykwm0lAAjBc/VgE-ZL3PeMI/AAAAAAAAAoY/-2PvWh7vKKU/s648/mcd_800x800.jpg",
-      rating: 4,
-      address:,
+      rating: 3,
+      address: '',
       comments: []
       
     },
     {
       name : "Pizza Hut",
       img: "https://lh4.googleusercontent.com/-Ykwm0lAAjBc/VgE-ZL3PeMI/AAAAAAAAAoY/-2PvWh7vKKU/s648/mcd_800x800.jpg",
-      rating: 4,
-      address:,
+      rating: 1,
+      address: '',
       comments: []
     }
 
