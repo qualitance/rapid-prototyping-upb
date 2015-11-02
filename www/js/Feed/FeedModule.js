a7732a6eb02857a1ebdaff9c3a5b61dee35053eb
cristian stanciulescu <cristian.stanciulescu@qualitance.com>
2015-11-02 13:30:50 +0200

    fix errors

--- www/js/Feed/FeedModule.js
+++ www/js/Feed/FeedModule.js
@@ -2,10 +2,13 @@ angular.module("FeedModule").
 	.config(function($stateProvider, $urlRouterProvider) {
 	 $stateProvider
 
-		  // setup an abstract state for the tabs directive
-		   .state('tab', {
-		    url: '/tab',
-		    abstract: true,
-		    templateUrl: 'templates/tabs.html'
-		  })
+	 .state('tab.tab-feed', {
+	      url: '/feed/',
+	      views: {
+	        'tab-feed': {
+	          templateUrl: 'templates/tab-feed.html',
+	          controller: 'FeedController'
+	        }
+	      }
+	    })
 });
--- www/js/services/appConfig.js
+++ www/js/services/appConfig.js
@@ -1,4 +1,4 @@
-angular.module('starter').factory("appConfig"), function(){
+angular.module('starter').factory("appConfig", function(){
 
 	return {
 		facebookAppId : '695010000635803'
