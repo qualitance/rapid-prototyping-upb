da1eaa4889cccaa5885b841d3eed265bf37e8b85
cristian stanciulescu <cristian.stanciulescu@qualitance.com>
2015-11-02 13:05:49 +0200

    first draft

---
+++ www/js/Feed/LoginModule.js
@@ -0,0 +1,11 @@
+angular.module("FeedModule").
+	.config(function($stateProvider, $urlRouterProvider) {
+	 $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+	}
